90aa86ae7289c5f54a8feb73ecd8d1c940e04d51
henriquelouz <henriquelouz@gmail.com>
2021-02-01 06:51:36 -0300

    we're done

--- README.md
+++
@@ -1,1 +0,0 @@
-# whitecockatiels.github.io
--- assets/js/player.js
+++
@@ -1,9 +0,0 @@
-$("#play").on("click",function(){
-    $(this).hide();
-    $("#pause").show();
-});
-
-$("#pause").on("click",function(){
-    $(this).hide();
-    $("#play").show();
-});
